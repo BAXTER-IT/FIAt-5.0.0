2f189f9e08a61c5f5da8126ddfe0c5823b14ed2a
Roziczky Árpád <arpi@baxter-it.com>
2025-05-30 15:26:29 +0200

    Refactor OrderBookController to use WebSocket for real-time order book updates instead of REST endpoints

--- binance-order-viewer/server/src/main/java/com/binance/orderviewer/controller/OrderBookController.java
+++ binance-order-viewer/server/src/main/java/com/binance/orderviewer/controller/OrderBookController.java
@@ -3,9 +3,9 @@ package com.binance.orderviewer.controller;
 import com.binance.orderviewer.model.OrderBook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class OrderBookController {
 
     private static final String ORDER_BOOK_TOPIC = "/topic/orderbook";
