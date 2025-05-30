907bdb5c16310dab46d293312a4e52bb800bcc4e
Roziczky Árpád <arpi@baxter-it.com>
2025-05-30 16:14:46 +0200

    Add .gitignore file and implement new features: API service, polyfills, and LimitController

---
+++ binance-order-viewer/client/src/polyfills.ts
@@ -0,0 +1,7 @@
+/**
+ * This file includes polyfills needed by Angular and is loaded before the app.
+ * You can add your own extra polyfills to this file.
+ */
+
+// Add global to window
+(window as any).global = window;
